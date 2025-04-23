c771ab731d2a9c3222af396b4ae83bc46acf4f9c
sangeetha <Artemis6161@github.com>
2025-04-23 21:24:44 +0530

    Add folder pages

---
+++ Frontend/resume-builder/src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to the Resume Builder</h1>
+      <p>Create and enhance your resume here!</p>
+    </div>
+  );
+}
+
+export default HomePage;
